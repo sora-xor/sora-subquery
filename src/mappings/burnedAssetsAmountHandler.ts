f99f63bd7c860bc01dc45f92fdfdcc4eff230c72
alexnatalia <alekseenko@soramitsu.co.jp>
2021-10-06 21:07:16 +0300

    Updated BurnedAssetsAmount handler.

--- src/mappings/burnedAssetsAmountHandler.ts
+++ src/mappings/burnedAssetsAmountHandler.ts
@@ -5,35 +5,19 @@ const XOR: string = '0x020000000000000000000000000000000000000000000000000000000
 const VAL: string = '0x0200040000000000000000000000000000000000000000000000000000000000';
 const PSWAP: string = '0x0200050000000000000000000000000000000000000000000000000000000000';
 
-let prevEventId: string = '';
-
-const updateBurnedValue = (
-    recordCurrencyId: string,
-    currencyId: string,
-    prevBurnedValue: string,
-    balance: string
-) => {
-    // TODO: Work with types
-    return recordCurrencyId === currencyId ? prevBurnedValue + balance : prevBurnedValue;
-    // return recordCurrencyId === currencyId ? prevBurnedValue + (balance as Balance).toBigInt() : prevBurnedValue;
-}
+let zeroBalance: string = '0';
 
 export async function handleBurnedAssetsAmount(event: SubstrateEvent): Promise<void> {
-    const { event: { data: [ id, block, accountId, currencyId, balance ] } } = event;
-    const record = new BurnedAssetsAmount(event.extrinsic.block.block.header.hash.toString());
-
-    record.id = id.toString();
-    // TODO: it's better to save by blockId
-    record.block = block.toString();
-    record.accountId = accountId.toString();
-    record.currencyId = currencyId.toString();
-    record.balance = balance.toString();
-    // TODO: Get burnedInfo from prev record and sum it with current value
-    const prevBurnedValues = await BurnedAssetsAmount.get(prevEventId)
-    record.burnedAmountXor = updateBurnedValue(record.currencyId, XOR, prevBurnedValues?.burnedAmountXor ?? '0', record.balance)
-    record.burnedAmountVal = updateBurnedValue(record.currencyId, VAL, prevBurnedValues?.burnedAmountVal ?? '0', record.balance)
-    record.burnedAmountPswap = updateBurnedValue(record.currencyId, PSWAP, prevBurnedValues?.burnedAmountPswap ?? '0', record.balance)
-    prevEventId = record.id;
-
-    await record.save();
+    logger.info(`event ${event.event.data}`);
+    const { event: { data: [ currencyId, , balance ] } } = event;
+    let burnedAssetsAmount: BurnedAssetsAmount = new BurnedAssetsAmount(event.event.hash.toString());
+    burnedAssetsAmount.blockHeight = event.block.block.header.number.toBigInt();
+    burnedAssetsAmount.blockHash = event.block.block.header.hash.toString();
+    burnedAssetsAmount.currencyId = currencyId.toString();
+    burnedAssetsAmount.balance = BigInt(balance.toString());
+    burnedAssetsAmount.burnedAmountXor = burnedAssetsAmount.currencyId === XOR ? burnedAssetsAmount.balance : BigInt(zeroBalance.toString());
+    burnedAssetsAmount.burnedAmountVal = burnedAssetsAmount.currencyId === VAL ? burnedAssetsAmount.balance : BigInt(zeroBalance.toString());
+    burnedAssetsAmount.burnedAmountPswap = burnedAssetsAmount.currencyId === PSWAP ? burnedAssetsAmount.balance : BigInt(zeroBalance.toString());
+    
+    await burnedAssetsAmount.save();
 }
