d3dd5092e1676a3148b0af5e7989aec1423bec9b
bgodlin <bogodline@gmail.com>
2021-10-22 10:11:44 +0300

    Fixed ETH->SORA handler

--- src/mappings/ethSoraTransferHandler.ts
+++ src/mappings/ethSoraTransferHandler.ts
@@ -1,6 +1,20 @@
 import {SubstrateEvent} from "@subql/types";
-import { HistoryElement } from "sora/types";
 import { formatU128ToBalance, assignCommonHistoryElemInfo } from "./utils";
+
+// Obtaining tokens for further transfer may be done by unlocking ("Transferred" event) or by minting ("Deposited"). Either way is a part of ETH->SORA transfer.
+
+const distinguishCurrenciesEvent = (currenciesEvent): string[] => {
+    switch (currenciesEvent.event.method) {
+        case "Deposited": {
+            const {event: {data: [assetId,,amount]}} = currenciesEvent
+            return [assetId, amount]
+        }
+        case "Transferred": {
+            const {event: {data: [assetId,,,amount]}} = currenciesEvent
+            return [assetId, amount]
+        }
+    }
+}
 
 export async function ethSoraTransferHandler(incomingRequestFinalizationEvent: SubstrateEvent): Promise<void> {
 
@@ -9,16 +23,17 @@ export async function ethSoraTransferHandler(incomingRequestFinalizationEvent: S
     const extrinsic = incomingRequestFinalizationEvent.extrinsic
     let registeredRequestEvent = extrinsic.events.find(e => e.event.method === 'RequestRegistered' && e.event.section === 'ethBridge')
     const {event: {data: [requestHash]}} = registeredRequestEvent
-    let currencyDepositEvent = extrinsic.events.find(e => e.event.method === 'Deposited' && e.event.section === 'currencies')
-    const {event: {data: [assetId,,amount]}} = currencyDepositEvent
+    let currenciesEvent = extrinsic.events.find(e => e.event.section === 'currencies')
+    let distinguishedCurrenciesEventValues = distinguishCurrenciesEvent(currenciesEvent)
 
     const record = assignCommonHistoryElemInfo(extrinsic)
+
     let entity = new Object();
 
     entity = {
         requestHash: requestHash.toString(),
-        assetId: assetId.toString(),
-        amount: formatU128ToBalance(amount.toString())
+        assetId: distinguishedCurrenciesEventValues[0].toString(),
+        amount: formatU128ToBalance(distinguishedCurrenciesEventValues[1].toString())
     }
 
     record.data = entity
