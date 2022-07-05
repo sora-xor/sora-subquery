0c69fe435a2470a2c3fd5c4d58b603c698bb9ea5
godlin <bogodline@gmail.com>
2022-07-05 13:03:21 +0200

    Fixed referral reserves

--- src/mappings/referralReserveHandler.ts
+++ src/mappings/referralReserveHandler.ts
@@ -3,15 +3,15 @@ import { formatU128ToBalance, assignCommonHistoryElemInfo } from "./utils";
 import { XOR } from "..";
 
 export async function referralReserveHandler(extrinsic: SubstrateExtrinsic): Promise<void> {
-	logger.debug("Caught referral reserve extrinsic");
+    logger.debug("Caught referral reserve extrinsic");
 
     const record = assignCommonHistoryElemInfo(extrinsic);
 
     let details = new Object();
 
-	if (record.execution.success) {
-        let referralReserveEvent = extrinsic.events.find(e => e.event.method === 'Transferred' && e.event.section === 'currencies');
-        const { event: { data: [, from, to, amount] } } = referralReserveEvent;
+    if (record.execution.success) {
+        let referralReserveEvent = extrinsic.events.find(e => e.event.method === 'Transfer' && e.event.section === 'balances');
+        const { event: { data: [from, to, amount] } } = referralReserveEvent;
 
         details = {
             from: from.toString(),
