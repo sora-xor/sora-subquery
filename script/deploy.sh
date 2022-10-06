a68a00702f8d7f2c0c903c14cba991fb4aae8962
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2022-10-06 13:45:09 +0300

    added script
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- script/deploy.sh
+++ script/deploy.sh
@@ -8,10 +8,3 @@ yarn
 yarn codegen
 RESULT=$(subql publish | grep -v 'SubQuery Project uploaded to IPFS: ')
 echo $RESULT
-subql deployment:deploy \
---useDefaults \
---org=${SUBQUERY_ORG} \
---ipfsCID=$RESULT \
---projectName=${matrix_chain} \
---endpoint=wss://ws.framenode-${endpoint}.sora2.soramitsu.co.jp
-echo "New deployment is executed"
