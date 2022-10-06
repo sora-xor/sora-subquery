f0be457e0a2056a652fecee28b6c88cb77651766
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2022-10-06 15:59:26 +0300

    new grep
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- script/deploy.sh
+++ script/deploy.sh
@@ -6,5 +6,12 @@ npm install tslib
 npm link typescript
 yarn
 yarn codegen
-RESULT=$(subql publish | grep -v 'SubQuery Project uploaded to IPFS: ')
+RESULT=$(subql publish | grep -oP ': \K.*')
 echo $RESULT
+subql deployment:deploy \
+--useDefaults \
+--org=sora-xor \
+--ipfsCID=${result} \
+--projectName=sora-dev \
+--endpoint=wss://ws.framenode-3.s3.dev.sora2.soramitsu.co.jp
+echo "New deployment is executed"
