5a94afae0dc718b4bd99db772773e320df738b7a
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2023-08-11 15:45:59 +0300

    fix to bash
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/deploy.sh
+++ housekeeping/deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 # deleteProject
-if [ $fullClean = true ]; then
+if [ "$fullClean" = true ]; then
     echo "👷‍♂️ Start deleting project..."
     subql project:delete-project \
     --org=$SUBQUERY_ORG \
@@ -11,22 +11,22 @@ if [ $fullClean = true ]; then
 fi
 
 # Create Project
-if [ $createProject = true ] || [ $fullClean = true ]; then
+if [ "$createProject" = true ] || [ "$fullClean" = true ]; then
     echo "👷‍♂️ Start creating project..."
     subql project:create-project \
     --org=$SUBQUERY_ORG \
     --dedicatedDB=$SUBQUERY_PROJECT_DB \
     --projectName=$project \
-    --gitRepo=https://github.com/sora-xor/sora-subquery
+    --gitRepo=https://github.com/sora-xor/sora-subquery ##
     echo "✅ project $project was created"
 fi
 
 # Deploying project
-if [ $prodslot = true ] || [ $createProject = true ] || [ $fullClean = true ] || [ $stageSlot = true ]; then
+if [ "$prodslot" = true ] || [ "$createProject" = true ] || [ "$fullClean" = true ] || [ "$stageSlot" = true ]; then
     yarn
     RESULT=$(subql publish -f project.yaml  | grep -oE ': \K.*')
     HASH=$(yarn config:chainId | grep -oE '0.*' | grep -v 's')
-    if [[ $prodslot = true || $createProject = true ]] || [[ $fullClean = true ]]; then
+    if [ "$prodslot" = true ] || [ "$createProject" = true ] || [ "$fullClean" = true ]; then
         echo "👷‍♂️ Deploying project in production slot..."
         sed -i '/chainId:/s/'0'/'$HASH'/' project.yaml
         sed -i '/startBlock:/s/1/'$STARTBLOCK'/' project.yaml
@@ -44,7 +44,7 @@ if [ $prodslot = true ] || [ $createProject = true ] || [ $fullClean = true ] ||
         --projectName=$project \
         --endpoint=$endpoint
         echo "✅ New deployment in production slot was executed from block from $STARTBLOCK block!"
-        elif [ $stageSlot = true ]; then
+        elif [ "$stageSlot" = true ]; then
         echo "👷‍♂️ Deploying project in staging slot..."
         sed -i '/chainId:/s/'0'/'$HASH'/' project.yaml
         sed -i '/startBlock:/s/1/'$STARTBLOCKSTG'/' project.yaml
