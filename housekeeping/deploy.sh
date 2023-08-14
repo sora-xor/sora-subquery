2d8116346ba8073446bc6821d322a32ebdfa7940
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2023-08-14 10:52:09 +0300

    buildCmd
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/deploy.sh
+++ housekeeping/deploy.sh
@@ -3,33 +3,34 @@ set -e
 
 SUBQUERY_CLI_VERSION="1.10.1"
 echo $INDEXER_VERSION_STAGING $stageSlot
+echo $SUBQL_ACCESS_TOKEN
 
 # deleteProject
 if [ "$fullClean" = true ]; then
-    echo "👷‍♂️ Start deleting project..."
+    printf "👷‍♂️ Start deleting project... \n"
     subql project:delete-project \
     --org=$SUBQUERY_ORG \
     --projectName=$project
-    echo "✅ project $project was deleted"
+    printf "✅ project $project was deleted \n"
 fi
 
 # Create Project
 if [ "$createProject" = true ] || [ "$fullClean" = true ]; then
-    echo "👷‍♂️ Start creating project..."
+    printf "👷‍♂️ Start creating project... \n"
     subql project:create-project \
     --org=$SUBQUERY_ORG \
     --dedicatedDB=$SUBQUERY_PROJECT_DB \
     --projectName=$project \
     --gitRepo=https://github.com/sora-xor/sora-subquery ##
-    echo "✅ project $project was created"
+    printf "✅ project $project was created \n"
 fi
 
 # Deploying project
 if [ "$prodslot" = true ] || [ "$createProject" = true ] || [ "$fullClean" = true ] || [ "$stageSlot" = true ]; then
-    printf 'Building project\n'
+    printf 'Building project \n'
     yarn
-    RESULT=$(subql publish -f project.yaml  | grep -oE ': \K.*')
-    HASH=$(yarn config:chainId | grep -oE '0.*' | grep -v 's')
+    RESULT='$(subql publish -f project.yaml  | grep -oE ': \K.*')'
+    HASH='$(yarn config:chainId | grep -oE '0.*' | grep -v 's')'
     if [ "$prodslot" = true ] || [ "$createProject" = true ] || [ "$fullClean" = true ]; then
         echo "👷‍♂️ Deploying project in production slot..."
         sed -i '/chainId:/s/'0'/'$HASH'/' project.yaml
@@ -47,7 +48,7 @@ if [ "$prodslot" = true ] || [ "$createProject" = true ] || [ "$fullClean" = tru
         --ipfsCID=$RESULT \
         --projectName=$project \
         --endpoint=$endpoint
-        echo "✅ New deployment in production slot was executed from block from $STARTBLOCK block!"
+        printf "✅ New deployment in production slot was executed from block from $STARTBLOCK block! \n"
         elif [ "$stageSlot" = true ]; then
         echo "👷‍♂️ Deploying project in staging slot..."
         sed -i '/chainId:/s/'0'/'$HASH'/' project.yaml
@@ -66,6 +67,6 @@ if [ "$prodslot" = true ] || [ "$createProject" = true ] || [ "$fullClean" = tru
         --projectName=$project \
         --endpoint=$endpoint \
         --type=stage
-        echo " ✅ New deployment in staging slot was executed from $STARTBLOCKSTG block!"
+        printf " ✅ New deployment in staging slot was executed from $STARTBLOCKSTG block! \n"
     fi
 fi
