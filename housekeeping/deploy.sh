1ff4c8420e6ad2b9106f31a2759a2c5fd37311b1
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2023-08-15 13:35:51 +0300

    revert old deploy
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/deploy.sh
+++ housekeeping/deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 set -e
 
 # vars
@@ -32,28 +32,6 @@ if [ "$createProject" = true ] || [ "$fullClean" = true ]; then
     printf "✅ project $project was created \n"
 fi
 
-
-deploy=(
-    yarn codegen
-    RESULT=$(subql publish -f project.yaml | grep -oE ': .*' | awk '{print $2}')
-    subql deployment:deploy \
-    --indexerVersion=v$indexer_version \
-    --queryVersion=v$query_version \
-    --org=$org \
-    --indexerSubscription \
-    --querySubscription \
-    --queryTimeout=20000 \
-    --disableHistorical \
-    --useDefaults \
-    --ipfsCID=$RESULT \
-    --projectName=$project \
-    --endpoint=$endpoint
-)
-
-if [ "$stageSlot" = true ]; then
-    deploy+=("--type=stage")
-fi
-
 # Deploying project
 if [ "$prodslot" = true ] || [ "$createProject" = true ] || [ "$fullClean" = true ] || [ "$stageSlot" = true ]; then
     printf 'Building project \n'
@@ -63,21 +41,42 @@ if [ "$prodslot" = true ] || [ "$createProject" = true ] || [ "$fullClean" = tru
         echo "👷‍♂️ Deploying project in production slot..."
         sed -i '/chainId:/s/'0'/'$HASH'/' project.yaml
         sed -i '/startBlock:/s/1/'$STARTBLOCK'/' project.yaml
-        #yarn codegen
-        #RESULT=$(subql publish -f project.yaml | grep -oE ': .*' | awk '{print $2}')
-        "${deploy[@]}"
+        yarn codegen
+        RESULT=$(subql publish -f project.yaml | grep -oE ': .*' | awk '{print $2}')
+        subql deployment:deploy \
+        --indexerVersion=v$indexer_version \
+        --queryVersion=v$query_version \
+        --org=$org \
+        --indexerSubscription \
+        --querySubscription \
+        --queryTimeout=20000 \
+        --disableHistorical \
+        --useDefaults \
+        --ipfsCID=$RESULT \
+        --projectName=$project \
+        --endpoint=$endpoint
+        $stage
         printf "✅ New deployment in production slot was executed from block from $STARTBLOCK block! \n"
-        if [ "$stageSlot" = true ]; then
-            printf "✅ New deployment in staging slot was executed from block from $STARTBLOCKSTG block! \n"
-        fi
     fi
     if [ "$stageSlot" = true ]; then
         echo "👷‍♂️ Deploying project in staging slot..."
         sed -i '/chainId:/s/'0'/'$HASH'/' project.yaml
         sed -i '/startBlock:/s/1/'$STARTBLOCKSTG'/' project.yaml
-        #yarn codegen
-        #RESULT=$(subql publish -f project.yaml | grep -oE ': .*' | awk '{print $2}')
-        "${deploy[@]}"
+        yarn codegen
+        RESULT=$(subql publish -f project.yaml | grep -oE ': .*' | awk '{print $2}')
+        subql deployment:deploy \
+        --indexerVersion=v$indexer_version \
+        --queryVersion=v$query_version \
+        --org=$org \
+        --useDefaults \
+        --indexerSubscription \
+        --querySubscription \
+        --queryTimeout=20000 \
+        --disableHistorical \
+        --ipfsCID=$RESULT \
+        --projectName=$project \
+        --endpoint=$endpoint \
+        --type=stage
         printf " ✅ New deployment in staging slot was executed from $STARTBLOCKSTG block! \n"
     fi
 fi
