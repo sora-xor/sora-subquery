850fae882bacd788cca38853f02c2e24a126e2c8
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2023-10-16 15:11:34 +0300

    add assets stats

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,14 @@
+import { SnapshotType } from "../types";
+
+import { SnapshotSecondsMap } from './consts';
+
 export const formatDateTimestamp = (date: Date): number => parseInt((date.getTime() / 1000).toFixed(0));
+
+export const getSnapshotIndex = (blockTimestamp: number, type: SnapshotType): { index: number, timestamp: number } => {
+  const seconds = SnapshotSecondsMap[type];
+  const index = Math.floor(blockTimestamp / seconds); // rounded snapshot index (from 0)
+  const timestamp = seconds * index; // rounded snapshot timestamp
+
+  return { index, timestamp };
+};
+
