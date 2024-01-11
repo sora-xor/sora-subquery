ef92ecee89a2f5943a317a1dd00d8bea060c0aae
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2024-01-11 21:14:43 +0300

    add composite indexes to snapshots

--- scripts/chainId.mjs
+++ scripts/chainId.mjs
@@ -1,17 +1,35 @@
-import { ApiPromise, WsProvider } from '@polkadot/api';
 import fs from 'fs-extra';
 import yaml from 'js-yaml';
+import fetch from 'node-fetch';
+
 const configPath = 'project.yaml';
 const shouldUpdate = process.argv.includes('--update');
 
-async function fetchChainId(endpoint) { 
-	const wsProvider = new WsProvider(endpoint);
-	const api = await ApiPromise.create({ provider: wsProvider });
-	const chainId = api.genesisHash.toHex();
-	await api.disconnect();
+async function fetchChainId(endpoint) {
+	const rpcUrl = endpoint.replace(/^ws(s)?:\/\/ws/, 'http$1://rpc');
+
+    // Fetch chainId using an RPC request
+    const request = {
+      id: 1,
+      jsonrpc: '2.0',
+      method: 'chain_getBlockHash',
+      params: [0],
+    };
+    const response = await fetch(rpcUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(request)
+    });
+    const responseData = await response.json();
+    const chainId = responseData.result;
+
 	console.log('Fetched chainId: ', chainId);
 	return chainId
-} async function main() { 
+}
+
+async function main() { 
 	try { 
 		// Read project.yaml 
 		const fileContents = await fs.readFile(configPath, 'utf8');
