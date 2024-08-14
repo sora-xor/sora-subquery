ccb257096c5806ecd68d41ab245933f35a2136d0
Kron1749 <70746258+Kron1749@users.noreply.github.com>
2024-08-14 16:29:15 +0300

    updated_with_defiR_set_sbt_expiration

--- src/mappings/calls/defiR.ts
+++ src/mappings/calls/defiR.ts
@@ -3,29 +3,21 @@ import { createHistoryElement } from "../../utils/history";
 import { getAssetId } from "../../utils/assets";
 import { logStartProcessingCall } from "../../utils/logs";
 
-export async function setSbtExpirationCallHandler(extrinsic: SubstrateExtrinsic): Promise<void> {
+export async function setSbtExpiratioCallHandler(extrinsic: SubstrateExtrinsic): Promise<void> {
 	logStartProcessingCall(extrinsic);
-
 	const { extrinsic: { args: [account, sbtAsset, newExpiresAt] } } = extrinsic as any;
 	
 	const sbtAssetId = getAssetId(sbtAsset);
+	const newExpiresAtTime = newExpiresAt.toString();
+	const accountId = account.toString();
 
 	const details = {
-		oldExpiresAt: undefined,
 		sbtAssetId,
-		account,
-		newExpiresAt
+		newExpiresAtTime,
+		accountId
 	}
 
-	const SBTExpirationUpdatedEvent = extrinsic.events.find(e => e.event.section === 'extendedAssets'
-		&& e.event.method === 'SBTExpirationUpdated');
-	
-	if (SBTExpirationUpdatedEvent) {
-		const { event: { data: [_sbtAssetId, _oldExpiresAt, _newExpiresAt] } } = SBTExpirationUpdatedEvent;
-
-    details.oldExpiresAt = _oldExpiresAt;
-	}
-	await createHistoryElement(extrinsic, details);
+	await createHistoryElement(extrinsic,details);
 }
 
 export async function regulateAssetCallHandler(extrinsic: SubstrateExtrinsic): Promise<void> { 
