98560ddb1a83a66b7c2b599c2048a3006cc8bd8d
Nikita Polyakov <53777036+Nikita-Polyakov@users.noreply.github.com>
2024-08-28 09:50:52 +0300

    fix chainId script (#550)

--- scripts/chainId.mjs
+++ scripts/chainId.mjs
@@ -5,35 +5,42 @@ import fetch from 'node-fetch';
 const configPath = 'project.yaml';
 const shouldUpdate = process.argv.includes('--update');
 
-function getRpcEndpoint(wsEndpoint) {
+function getRpcEndpoint(wsEndpoint, isSub5 = false) {
 	return wsEndpoint.replace(/^ws(s)?:\/\/(ws)?/, (_, p1, p2) => {
 		let str = 'http';
 		if (p1) str += p1;
 		str += '://';
-		if (p2) str += 'rpc';
+		if (p2) str += isSub5 ? p2 : 'rpc';
 		return str;
 	})
 }
 
-async function fetchChainId(endpoint) {
-	const rpcUrl = getRpcEndpoint(endpoint);
+async function request(url, body) {
+  return await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body
+  });
+};
 
-    // Fetch chainId using an RPC request
-    const request = {
-      id: 1,
-      jsonrpc: '2.0',
-      method: 'chain_getBlockHash',
-      params: [0],
-    };
-    const response = await fetch(rpcUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(request)
-    });
-    const responseData = await response.json();
-    const chainId = responseData.result;
+async function fetchChainId(endpoint) {
+	const sub4Url = getRpcEndpoint(endpoint, false);
+  const sub5Url = getRpcEndpoint(endpoint, true);
+  const urls = [sub4Url, sub5Url];
+
+  // Fetch chainId using an RPC request
+  const body = JSON.stringify({
+    id: 1,
+    jsonrpc: '2.0',
+    method: 'chain_getBlockHash',
+    params: [0],
+  });
+
+  const response = await Promise.any(urls.map((url) => request(url, body)));
+  const responseData = await response.json();
+  const chainId = responseData.result;
 
 	console.log('Fetched chainId: ', chainId);
 	return chainId
