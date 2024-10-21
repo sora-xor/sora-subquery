1fa6ba715bc163ddc465a95ab3686af4ac12029c
Nikita Polyakov <53777036+Nikita-Polyakov@users.noreply.github.com>
2024-10-21 14:41:35 +0300

    [PROD] Prices v2 (#570)
    * prices v2 aligned price impact
    
    * Update project-onfinality.yaml
    
    * Update project.yaml
    
    * Update project-onfinality.yaml
    
    * Update project-onfinality.yaml
    
    * Update Jenkinsfile-onfinality
    
    * Update Jenkinsfile-onfinality
    
    * Update Jenkinsfile-onfinality
    
    * Update project-onfinality.yaml
    
    * Update Jenkinsfile-onfinality
    
    * prices based on dai swap
    
    * fix amount
    
    * update start block
    
    * fix init assets
    
    * update prices v2 start block in config to 18_020_000
    
    * return project.yaml & jenkins settings
    
    ---------
    
    Co-authored-by: f33r0 <95526886+f33r0@users.noreply.github.com>

--- src/mappings/calls/band.ts
+++ src/mappings/calls/band.ts
@@ -2,9 +2,14 @@ import { SubstrateExtrinsic } from '@subql/types';
 
 import { tickerSyntheticAssetId, assetSnapshotsStorage, formatU128ToBalance } from '../../utils/assets';
 import { bytesToString, getExtrinsicArgs } from '../../utils';
-import { getCallHandlerLog, logStartProcessingCall } from '../../utils/logs';
+import { BASE_ASSETS } from '../../utils/consts';
+import { getCallHandlerLog, logStartProcessingCall } from "../../utils/logs";
+import { poolAccounts } from '../../utils/pools';
+import { isPriceV2 } from "../../utils/price";
 
 export async function handleBandRateUpdate(extrinsic: SubstrateExtrinsic): Promise<void> {
+  const blockNumber = extrinsic.block.block.header.number.toNumber();
+
   logStartProcessingCall(extrinsic);
 
   const [rates] = getExtrinsicArgs(extrinsic) as any;
@@ -13,14 +18,24 @@ export async function handleBandRateUpdate(extrinsic: SubstrateExtrinsic): Promi
     const referenceSymbol = bytesToString(ticker);
     const syntheticAssetId = tickerSyntheticAssetId.get(referenceSymbol);
 
-    if (syntheticAssetId) {
+    if (!syntheticAssetId) continue;
+
+    let price = '0';
+
+    if (isPriceV2(blockNumber)) {
+      const hasPool = BASE_ASSETS.some((baseAssetId) => {
+        return !!poolAccounts.getMap(baseAssetId)?.has(syntheticAssetId);
+      });
+      // the price must be updated from pool reserves, otherwise it will be zero.
+      if (hasPool) return;
+    } else {
       const u64 = rate.toString();
       const u128 = u64 + '0'.repeat(9);
-      const price = formatU128ToBalance(u128, syntheticAssetId);
-
-      getCallHandlerLog(extrinsic).debug({ syntheticAssetId, price }, 'Synthetic asset price update');
-
-      await assetSnapshotsStorage.updatePrice(extrinsic.block, syntheticAssetId, price);
+      price = formatU128ToBalance(u128, syntheticAssetId);
     }
+
+    await assetSnapshotsStorage.updatePrice(extrinsic.block, syntheticAssetId, price);
+
+    getCallHandlerLog(extrinsic).debug({ syntheticAssetId, price }, 'Synthetic asset price update')
   }
 }
