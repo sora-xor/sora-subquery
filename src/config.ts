352c9cecb2168dc3599ebaf557058dfd0b736076
Nikita Polyakov <53777036+Nikita-Polyakov@users.noreply.github.com>
2024-10-24 15:34:33 +0300

    Add point system model (#573)
    * add account point system
    
    * update config
    
    * update check

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,6 @@
 export const testLogMode = process.env.INDEXER_TEST_LOG_MODE ? process.env.INDEXER_TEST_LOG_MODE === 'true' : true;
 
 export const PricesV2StartBlock = 1;
+
+export const PointSystemStartBlockV1 = 1;
+export const PointSystemStartBlockV2 = 1;
