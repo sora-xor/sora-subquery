b22269a7d40d0d6477d453277a8e10dbb9c76581
Rustem <rustem.yuzlibaev@yandex.ru>
2024-12-16 12:35:48 +0500

    add PRUSD (#604)
    * Fix XORless events parsing
    
    * add PRUSD
    
    * add PRUSD
    
    * format
    
    * Update tsconfig.tsbuildinfo
    
    ---------
    
    Co-authored-by: Vladimir Stepanenko <vovac12@gmail.com>

--- src/mappings/events/networkFee.ts
+++ src/mappings/events/networkFee.ts
@@ -3,20 +3,22 @@ import { SubstrateEvent } from '@subql/types';
 import { XOR } from '../../utils/consts';
 import { getAmountUSD, formatU128ToBalance } from '../../utils/assets';
 import { accountMetaStorage } from '../../utils/account';
-import { getEventData } from '../../utils/events';
+import { getFeeWithdrawnData } from '../../utils/events';
 import { networkSnapshotsStorage } from '../../utils/network';
 import { logStartProcessingEvent } from '../../utils/logs';
 
 export async function handleNetworkFee(event: SubstrateEvent): Promise<void> {
   logStartProcessingEvent(event);
 
-  const [account, fee] = getEventData(event);
+  const { accountId, fee, assetId } = getFeeWithdrawnData(event);
 
-  const accountId = account.toString();
-  const assetId = XOR;
-  const amount = formatU128ToBalance(fee.toString(), assetId);
+  if (assetId !== XOR) {
+    throw new Error('XORless transactions is not supported!');
+  }
+
+  const amount = formatU128ToBalance(fee, assetId);
   const amountUSD = await getAmountUSD(event.block, assetId, amount);
 
   await accountMetaStorage.updateFees(event.block, accountId, amount, amountUSD);
-  await networkSnapshotsStorage.updateFeesStats(event.block, BigInt(fee.toString()));
+  await networkSnapshotsStorage.updateFeesStats(event.block, BigInt(fee));
 }
