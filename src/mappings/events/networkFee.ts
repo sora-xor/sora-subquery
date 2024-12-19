e82da4811cb9fb629296d7f5709c4c434e8e1157
Nikita Polyakov <53777036+Nikita-Polyakov@users.noreply.github.com>
2024-12-19 15:46:34 +0300

    fix xorless fee (#613)

--- src/mappings/events/networkFee.ts
+++ src/mappings/events/networkFee.ts
@@ -3,7 +3,7 @@ import { SubstrateEvent } from '@subql/types';
 import { XOR } from '../../utils/consts';
 import { getAmountUSD, formatU128ToBalance } from '../../utils/assets';
 import { accountMetaStorage } from '../../utils/account';
-import { getFeeWithdrawnData } from '../../utils/events';
+import { getFeeWithdrawnData, isEvent, getEventData } from '../../utils/events';
 import { networkSnapshotsStorage } from '../../utils/network';
 import { logStartProcessingEvent } from '../../utils/logs';
 
@@ -12,13 +12,23 @@ export async function handleNetworkFee(event: SubstrateEvent): Promise<void> {
 
   const { accountId, fee, assetId } = getFeeWithdrawnData(event);
 
+  let xorFee = fee;
+
   if (assetId !== XOR) {
-    throw new Error('XORless transactions is not supported!');
+    xorFee = '0';
+
+    const feePayment = event.extrinsic?.events.find((e) => isEvent(e, 'transactionPayment', 'TransactionFeePaid'));
+
+    if (feePayment) {
+      const [_who, actualFee, _tip] = getEventData(feePayment);
+
+      xorFee = actualFee.toString();
+    }
   }
 
-  const amount = formatU128ToBalance(fee, assetId);
-  const amountUSD = await getAmountUSD(event.block, assetId, amount);
+  const amount = formatU128ToBalance(xorFee, XOR);
+  const amountUSD = await getAmountUSD(event.block, XOR, amount);
 
   await accountMetaStorage.updateFees(event.block, accountId, amount, amountUSD);
-  await networkSnapshotsStorage.updateFeesStats(event.block, BigInt(fee));
+  await networkSnapshotsStorage.updateFeesStats(event.block, BigInt(xorFee));
 }
